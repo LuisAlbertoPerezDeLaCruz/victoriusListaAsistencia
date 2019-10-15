38717c35af344dc0de32b5659b70dc85b4748a24
Luis Pérez <laperezdelacruz@gmail.com>
2019-10-15 06:05:05 -0400

    agregada navegacion al detalle de la actividad

--- src/app/pages/actividades/actividades.page.ts
+++ src/app/pages/actividades/actividades.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-actividades',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActividadesPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  openDetails() {
+    // Both of these would work!
+    // But the standard Router is recommended.
+    // this.navController.navigateForward(`/tabs/films/42`);
+    this.router.navigateByUrl(`/tabs/actividades/42`);
   }
 
 }
